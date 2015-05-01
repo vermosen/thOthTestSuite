876ad90517bd9102a03fea20a1988cf04ef1c3c2
vermosen <jeanmathieu.vermosen@rutgers.edu>
2015-05-01 17:27:47 -0400

    minor changes

--- src/signalTest/signalTest1.hpp
+++ src/signalTest/signalTest1.hpp
@@ -8,15 +8,37 @@
 #ifndef SIGNALTEST1_HPP_
 #define SIGNALTEST1_HPP_
 
+#include <thOth/types.hpp>
 #include <thOth/pattern/observer2.hpp>
 #include <thOth/pattern/observable2.hpp>
 
-// a function to test signals
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+
+// a sender
+void sendingLoopInt(int seed, thOth::observable2<thOth::real> & send) {
+
+	for (int i = seed;; i++) send(i);
+
+};
+
 void signalTest1() {
 
-	thOth::observer2<int> tt();
+	thOth::observable2<thOth::real> send;
+	thOth::observer2<thOth::real> rec;
+
+	std::cout << "this thread id: " << boost::this_thread::get_id();
+
+	send.registerWith(rec);
+
+	// create the sender threads
+	boost::thread sendingThread1(sendingLoopInt, 1,   boost::ref(send));
+	boost::thread sendingThread2(sendingLoopInt, 100, boost::ref(send));
+
+	// join the threads
+	sendingThread1.join();
+	sendingThread2.join();
 
 }
-
 
 #endif /* SIGNALTEST1_HPP_ */
